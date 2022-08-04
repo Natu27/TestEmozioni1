6c67ece7f5d4043a6572810fd0c8551cc7297446
loren <lorenzonaturale7@gmail.com>
2022-08-04 12:05:03 +0200

    Username univoco

--- src/emotionalsongs/UtenteManager.java
+++ src/emotionalsongs/UtenteManager.java
@@ -3,6 +3,7 @@ package emotionalsongs;
 import prog.io.ConsoleInputManager;
 import prog.io.ConsoleOutputManager;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -30,6 +31,49 @@ public class UtenteManager {
         return arrUtenti;
     }
 
+    public static ArrayList<String> leggiUsername() throws IOException, ClassNotFoundException {
+        Object ob = FileManager.leggiFile(path);
+        ArrayList<Utente> arrUtenti = new ArrayList<Utente>();
+        if(ob instanceof ArrayList<?>) {
+            ArrayList<?> tmp = (ArrayList<?>) ob;
+            arrUtenti = castArrayUtenti(tmp);
+        }
+        ArrayList<String> arrUsername = new ArrayList<String>();
+        for(Utente utente : arrUtenti) {
+            arrUsername.add(utente.getUserId());
+        }
+        return arrUsername;
+    }
+
+    public static boolean usernameUtilizzato(String userInserito, ArrayList<String> usernameUtilizzati) {
+        ConsoleOutputManager out = new ConsoleOutputManager();
+        boolean userUtilizzato = false;
+        File file = new File("src/DATA/UtentiRegistrati.txt");
+        if(file.length() != 0) {
+            for (String username : usernameUtilizzati) {
+                if (username.equals(userInserito)) {
+                    userUtilizzato = true;
+                    out.println("Username già utilizzato");
+                    break;
+                }
+            }
+        }
+        return userUtilizzato;
+    }
+
+    public static String controllUsername(String s) throws IOException, ClassNotFoundException {
+        ConsoleInputManager in = new ConsoleInputManager();
+        File file = new File("src/DATA/UtentiRegistrati.txt");
+        ArrayList<String> userUtilizzati = new ArrayList<String>();
+        if(file.length() != 0) {
+            userUtilizzati = UtenteManager.leggiUsername();
+        }
+        s = s.trim();
+        while(s.equals("") || RicercaCanzone.everyCharWhitespace(s) || usernameUtilizzato(s, userUtilizzati))
+            s = in.readLine("VALORE NON CONSENTITO - Reinserire: ");
+        return s;
+    }
+
     public static ArrayList<Utente> castArrayUtenti(ArrayList<?> arrProv) {
         ArrayList<Utente> arrUtenti = new ArrayList<Utente>();
         for(Object ob : arrProv) {
@@ -40,7 +84,7 @@ public class UtenteManager {
         return arrUtenti;
     }
 
-    public static Utente registraUtente() {
+    public static Utente registraUtente() throws IOException, ClassNotFoundException {
         ConsoleInputManager in = new ConsoleInputManager();
         ConsoleOutputManager out = new ConsoleOutputManager();
         String nome = in.readLine("Inserisci nome: ");
@@ -81,9 +125,9 @@ public class UtenteManager {
         //out.println(indMail);
         out.println("DATI UTENTE: " + nomeCognome + " - " + codFiscale + " - " + indFisico + " - " + indMail);
         String username = in.readLine("Inserisci Username: ");
-        RicercaCanzone.controll(username);
+        username = controllUsername(username);
         String password = in.readLine("Inserisci Password: ");
-        RicercaCanzone.controll(password);
+        password = RicercaCanzone.controll(password);
         Utente nuovoUtente = new Utente(nomeCognome, codFiscale, indFisico, indMail, username, password);
         return nuovoUtente;
     }
