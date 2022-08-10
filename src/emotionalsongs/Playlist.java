37aff685dc0884f5eb3d18f8d21afd860c5887c8
loren <lorenzonaturale7@gmail.com>
2022-08-10 10:45:35 +0200

    Creazione File Playlist.dati

--- src/emotionalsongs/Playlist.java
+++ src/emotionalsongs/Playlist.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Playlist implements Serializable {
 
+    String nomePlaylist;
     ArrayList<Canzone> arrCanzoni = new ArrayList<Canzone>();
     Utente utente;
 }
