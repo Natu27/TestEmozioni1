ea80e8cd485977813c54bb348a5ce0dc7d618e85
Riccardo Grazioli <grazioli.riccardo02@gmail.com>
2022-08-27 18:58:46 +0200

    Aggiunta canzoni nel file CanzoniProv... (fino alla lettera L). manca la serializzazione

--- src/emotionalsongs/PathManager.java
+++ src/emotionalsongs/PathManager.java
@@ -12,9 +12,9 @@ public class PathManager {
     public static String getPath(PathType tipo) {
         String result = "";
         if(System.getProperty("os.name").startsWith("W")) {
-            result = "C:/Users/" + System.getProperty("user.name") + "/IdeaProjects/TestEmozioni/out/production/TestEmozioni/DATA/";
+            result = "C:/Users/" + System.getProperty("user.name") + "/Desktop/Università/LAB.A/TestEmozioni1/out/production/TestEmozioni/DATA/";
         } else {
-            result = System.getProperty("user.name") + "$/Users/IdeaProjects/TestEmozioni/out/production/TestEmozioni/DATA/";
+            result = System.getProperty("user.name") + "$/Users/Desktop/Università/LAB.A/TestEmozioni1/out/production/TestEmozioni/DATA/";
         }
         //String result = "C:/Users/loren%sIdeaProjects/TestEmozioni/out/production/TestEmozioni/DATA/"; //prima di consegnarlo alla prof metto "..%sDATA%s" al posto di src....
         //String result = System.getProperty("user.dir") + "\\out\\production\\TestEmozioni\\DATA\\";
